7abfc4c6a067bc891a608a152fed77d316292301
Shimon Israel <sisrael@bayareametro.gov>
2018-11-19 15:51:37 -0800

    File renaming and deleting

--- Analysis/ACS PUMS 2012-2016/CASA/ACS 2012-2016 PUMS Persons by Poverty Status and Minority and Housing Expense.R
+++ Analysis/ACS PUMS 2012-2016/CASA/ACS 2012-2016 PUMS Persons by Poverty Status and Minority and Housing Expense.R
@@ -1,47 +1,60 @@
 # ACS 2012-2016 PUMS Persons by Poverty Status and Minority and Housing Expense.R
-# Analyze PUMS data for Person Income Categories and By Mode, Minority and Age Status, 2016 1-year PUMS data
+# Analyze PUMS data for poverty status by minority and housing expense, 2016 5-year PUMS data
 # Import Libraries
 
 suppressMessages(library(dplyr))
 
 # Input occupied household and person census files
 
-HOUSEHOLD_OCC_RDATA = "M:/Data/Census/PUMS/PUMS 2016/hbayarea16_occ.Rdata"
-PERSON_RDATA = "M:/Data/Census/PUMS/PUMS 2016/pbayarea16.Rdata"
-OUTPUT = "M:/Data/Requests/Mallory Atkinson/TIP 2018/"
+HOUSEHOLD_OCC_RDATA = "M:/Data/Census/PUMS/PUMS 2012-16/hbayarea1216_occ.Rdata"
+PERSON_RDATA = "M:/Data/Census/PUMS/PUMS 2012-16/pbayarea1216.Rdata"
+OUTPUT = "M:/Data/Requests/Vikrant Sood/CASA/"
 
 load (HOUSEHOLD_OCC_RDATA)
 load (PERSON_RDATA)
 
-person <- left_join(pbayarea16,hbayarea16_occ,by=c("SERIALNO","PUMA","COUNTY","County_Name","ADJINC")) %>%
- filter(!is.na(TEN)) %>%                                                    # Remove GQ folks for HHs only, allowing income calculation
-  select(SERIALNO,PUMA,COUNTY,County_Name,ADJINC,HINCP,PWGTP,JWTR,JWRIP,TEN,HISP,RAC1P,AGEP) %>% mutate(
-    adjustedinc=HINCP*(ADJINC/1000000)
-  )
+# Join household and person files, sort by minority,income/poverty ratio, share of income on housing
+# Create intersection of minority,less than 200 percent income/poverty, and greater than 30 percent income on housing
+# Do the same for greater than 50 percent spent on housing
+# If conditions create intersection of all those variables, assign PUMS weight, otherwise assign zero
 
-person1 <- person %>% 
-  filter(JWTR!="") %>% mutate(
-  LT50=if_else(adjustedinc<50000,PWGTP,0L),
-  Total=PWGTP,
-  Minority=if_else((HISP>1 | RAC1P>1),PWGTP,0L),
-  Senior=if_else(AGEP>=65,PWGTP,0L),
-  JTW=if_else(JWTR==1 & JWRIP==1,"Roadway(Motorized)",
-              if_else(JWTR==1 & JWRIP==2, "Roadway(Motorized)",
-                      if_else(JWTR==1 & JWRIP>=3, "Roadway(Motorized)",
-                              if_else(JWTR>=2 & JWTR<=6,"Transit",
-                                      if_else(JWTR==7 | JWTR==8,"Roadway(Motorized)",
-                                              if_else(JWTR==9,"Roadway(Non-Motorized)",
-                                                      if_else(JWTR==10,"Roadway(Non-Motorized)",
-                                                              if_else(JWTR==11,"AtHome",
-                                                                      if_else(JWTR==12,"Other","Mistake")))))))))
-)
 
-sum.total <- person1 %>%
-  group_by(JTW) %>%
-  summarize(freq = n(), LT50=sum(LT50), Minority=sum(Minority),Senior=sum(Senior),Total=sum(Total))
+person <- left_join(pbayarea1216,hbayarea1216_occ,by=c("SERIALNO","PUMA","COUNTY","County_Name")) %>%
+ filter(TEN==3) %>% # Include only renters
+  select(SERIALNO,PUMA,COUNTY,County_Name,GRPIP,POVPIP,PWGTP,TEN,HISP,RAC1P,HINCP) %>%
+  mutate(
+    total=PWGTP,
+    race=if_else((HISP>1 | RAC1P>1),"minority","white"),
+    povshare=case_when(
+      (POVPIP>=0 & POVPIP<200) ~ "lt200",
+      POVPIP>=200 ~ "gt200"
+    ),
+    rentshare=case_when(
+      (GRPIP>=1 & GRPIP<30) ~ "from1_29",
+      (GRPIP>=30 & GRPIP<50) ~ "from30_50",
+      (GRPIP>=50) ~ "gt50"
+      ),
+    intersection_gt30=if_else((race=="minority" & povshare=="lt200" & (rentshare=="from30_50" | rentshare=="gt50")),
+                              PWGTP,0L),
+    intersection_gt50=if_else((race=="minority" & povshare=="lt200" & (rentshare=="gt50")),
+                              PWGTP,0L))
 
-write.csv(sum.total, paste0(OUTPUT, "ACSPUMS2016_Persons_HHIncome_Mode_Minority_Senior.csv"), row.names = FALSE, quote = T)
+# Summarize greater than 30, greater than 50, and totals
 
+sum.person <- person %>%
+  mutate(
+    intersection_gt30=if_else(is.na(intersection_gt30),0L,intersection_gt30),    # Change NA values to 0, as these PUMA
+    intersection_gt50=if_else(is.na(intersection_gt50),0L,intersection_gt50)     # records should be included for totals
+  ) %>%
+  group_by(PUMA) %>%
+  summarize(freq = n(), intersection_gt30=sum(intersection_gt30), intersection_gt50=sum(intersection_gt50),
+            total=sum(total)) %>%
+  ungroup() %>%
+  mutate(sharegt30=intersection_gt30/total,sharegt50=intersection_gt50/total)
+
+# Output csv
+
+write.csv(sum.person, paste0(OUTPUT, "ACSPUMS2012-2016_Persons_Minority_Rent_Poverty_Expense.csv"), row.names = FALSE, quote = T)
 
 
  
