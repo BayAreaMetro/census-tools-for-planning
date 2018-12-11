5d686805a872c17eb4cf728f946b2dc629c24e61
Shimon Israel <sisrael@bayareametro.gov>
2018-12-11 08:38:53 -0800

    Find value for 2017 Q2/Q3 HH split
    For use with 2015 TAZ land use data development.

--- Analysis/ACS PUMS 2012-2016/Share HHs Income 75-100k/ACS 2012-2016 PUMS 75-100k HH Distribution.R
+++ Analysis/ACS PUMS 2012-2016/Share HHs Income 75-100k/ACS 2012-2016 PUMS 75-100k HH Distribution.R
@@ -10,9 +10,9 @@ HOUSEHOLD_RDATA = "M:/Data/Census/PUMS/PUMS 2012-16/hbayarea1216_occ.Rdata"
 
 load (HOUSEHOLD_RDATA)
 
-# Keep only needed variables
+# Keep only needed variables, find share for 2016$
 
-household <- hbayarea1216_occ %>%
+household_16 <- hbayarea1216_occ %>%
   select(PUMA, HINCP, ADJINC, WGTP) %>%
   mutate(adjustedinc=HINCP*(ADJINC/1000000)) %>%
   filter(adjustedinc>=75000 & adjustedinc<99999) %>%
@@ -21,8 +21,25 @@ household <- hbayarea1216_occ %>%
     below88681=if_else(adjustedinc<88681,WGTP,0L)
     )
 
-sum_household <- household %>%
+sum_household_16 <- household_16 %>%
   summarize(above=sum(above88681), below=sum(below88681), total=sum(WGTP)) %>% 
   mutate(shareabove=above/total)
 
-print(sum_household$shareabove)
+print(sum_household_16$shareabove)
+
+# Keep only needed variables, find share for 2017$
+
+household_17 <- hbayarea1216_occ %>%
+  select(PUMA, HINCP, ADJINC, WGTP) %>%
+  mutate(adjustedinc=HINCP*(ADJINC/1000000)) %>%
+  filter(adjustedinc>=75000 & adjustedinc<99999) %>%
+  mutate(
+    above91538=if_else(adjustedinc>=91538,WGTP,0L),
+    below91538=if_else(adjustedinc<91538,WGTP,0L)
+  )
+
+sum_household_17 <- household_17 %>%
+  summarize(above=sum(above91538), below=sum(below91538), total=sum(WGTP)) %>% 
+  mutate(shareabove=above/total)
+
+print(sum_household_17$shareabove)
