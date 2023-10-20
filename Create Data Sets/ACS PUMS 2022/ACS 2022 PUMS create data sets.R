ae0c9904fa09d3072aa187ef4112660274ffe2e3
Shimon Israel <sisrael@bayareametro.gov>
2023-10-20 09:36:28 -0700

    Added CSV and feather outputs.

--- Create Data Sets/ACS PUMS 2022/ACS 2022 PUMS create data sets.R
+++ Create Data Sets/ACS PUMS 2022/ACS 2022 PUMS create data sets.R
@@ -4,6 +4,7 @@
 # Import Libraries
 
 suppressMessages(library(dplyr))
+suppressMessages(library(feather))
 
 # Set up directories with csv files for PUMS data
 
@@ -17,6 +18,9 @@ CAPER_OUTPUT_RDATA = "M:/Data/Census/PUMS/PUMS 2022/pcalif22.Rdata"
 BAYHH_OUTPUT_RDATA   = "M:/Data/Census/PUMS/PUMS 2022/hbayarea22.Rdata"
 BAYPER_OUTPUT_RDATA = "M:/Data/Census/PUMS/PUMS 2022/pbayarea22.Rdata"
 
+BAYHH_OUTPUT_CSV = str_replace(BAYHH_OUTPUT_RDATA, ".Rdata", ".csv")
+BAYPER_OUTPUT_CSV = str_replace(BAYPER_OUTPUT_RDATA, ".Rdata", ".csv")
+
 hcalif22 <- read.csv(CA_HOUSEHOLDS, colClasses = c(PUMA = "character",ST = "character"))
 pcalif22 <- read.csv(CA_PERSONS, colClasses = c(PUMA = "character",ST = "character"))
 equivalence <- read.csv(EQUIVALENCE, colClasses = c(PUMARC = "character",State = "character"))
@@ -24,10 +28,21 @@ equivalence <- read.csv(EQUIVALENCE, colClasses = c(PUMARC = "character",State =
 hbayarea22 <- merge (hcalif22,equivalence, by.x=c("ST","PUMA"), by.y=c("State","PUMARC"))
 pbayarea22 <- merge (pcalif22,equivalence, by.x=c("ST","PUMA"), by.y=c("State","PUMARC"))
 
-# Save out files
+# Save out files in R format
 
 save(hcalif22, file = CAHH_OUTPUT_RDATA)
 save(pcalif22, file = CAPER_OUTPUT_RDATA)
 
 save(hbayarea22, file = BAYHH_OUTPUT_RDATA)
 save(pbayarea22, file = BAYPER_OUTPUT_RDATA)
+
+write.csv(hbayarea22, file = BAYHH_OUTPUT_CSV, row.names=FALSE)
+write.csv(pbayarea22, file = BAYPER_OUTPUT_CSV, row.names=FALSE)
+
+# feather
+
+BAYHH_OUTPUT_FEATHER = str_replace(BAYHH_OUTPUT_RDATA, ".Rdata", ".feather")
+BAYPER_OUTPUT_FEATHER = str_replace(BAYPER_OUTPUT_RDATA, ".Rdata", ".feather")
+
+write_feather(hbayarea1721, BAYHH_OUTPUT_FEATHER)
+write_feather(pbayarea1721, BAYPER_OUTPUT_FEATHER)
