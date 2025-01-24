538a2936de8bd4f10b339876a82ffb57514d14c5
Shimon Israel <sisrael@bayareametro.gov>
2025-01-24 10:09:41 -0800

    Finished disability variable recoding

--- analysis_by_topic/PBA50p_Demographic_Update.R
+++ analysis_by_topic/PBA50p_Demographic_Update.R
@@ -14,8 +14,8 @@ PBA50p_dir         <- file.path(USERPROFILE, "Box", "Plan Bay Area 2050+","Perfo
 
 ## Set ACS variables
 
-ACS_year    = "2022" # Set ACS year
-ACS_product = "acs5" # Set ACS 1- or 5-year dataset
+acs_year    =  2022  # Set ACS year
+acs_product = "acs5" # Set ACS 1- or 5-year dataset
 
 ## Geography for Bay Area counties and California state
 
@@ -59,7 +59,7 @@ med_dis_earnings <- c(med_dis_earnings_        =    "B18140_002",  # Median earn
 # First total then iterated by race/ethnicity
 # Note that one should use Table B18101H for "white" because it omits Hispanic/Latino while Table B18101A includes Hispanic/Latino
 
-# Total Disability
+# Total disability
 
 disability <- c(tot_dis_universe_              =    "B18101_001",  # Disability universe - Civilian noninstitutionalized population
                 male_dis_under5_               =    "B18101_004",  # Male under 5 with a disability
@@ -76,10 +76,91 @@ disability <- c(tot_dis_universe_              =    "B18101_001",  # Disability 
                 female_dis_65_74_              =    "B18101_035",  # Female 65 to 74 with a disability
                 female_dis_75p_                =    "B18101_038",  # Female 75 plus with a disability
 
-# White, Not Hispanic/Latino Disability
+# White, not Hispanic/Latino disability
 
                 white_dis_universe_            =    "B18101H_001", # Total white alone, not Hispanic/Latino civilian noninstitutionalized population
                 white_dis_under18_             =    "B18101H_003", # White under 18 with a disability
                 white_dis_18_64_               =    "B18101H_006", # White 18 to 64 with a disability
-                white_dis_65p                  =    "B18101H_009", # White 65 plus with a disability
+                white_dis_65p_                 =    "B18101H_009", # White 65 plus with a disability
+
+# Black disability
+
+                black_dis_universe_            =    "B18101B_001", # Total Black civilian noninstitutionalized population
+                black_dis_under18_             =    "B18101B_003", # Black under 18 with a disability
+                black_dis_18_64_               =    "B18101B_006", # Black 18 to 64 with a disability
+                black_dis_65p_                 =    "B18101B_009", # Black 65 plus with a disability
+
+# Asian disability
+
+                asian_dis_universe_            =    "B18101D_001", # Total Asian civilian noninstitutionalized population
+                asian_dis_under18_             =    "B18101D_003", # Asian under 18 with a disability
+                asian_dis_18_64_               =    "B18101D_006", # Asian 18 to 64 with a disability
+                asian_dis_65p_                 =    "B18101D_009", # Asian 65 plus with a disability
+
+# Hispanic disability
+
+                hispanic_dis_universe_         =    "B18101I_001", # Total Hispanic civilian noninstitutionalized population
+                hispanic_dis_under18_          =    "B18101I_003", # Hispanic under 18 with a disability
+                hispanic_dis_18_64_            =    "B18101I_006", # Hispanic 18 to 64 with a disability
+                hispanic_dis_65p_              =    "B18101I_009", # Hispanic 65 plus with a disability
+
+# American Indian and Alaska Native (AIAN) disability
+
+                aian_dis_universe_             =    "B18101C_001", # Total AIAN civilian noninstitutionalized population
+                aian_dis_under18_              =    "B18101C_003", # AIAN under 18 with a disability
+                aian_dis_18_64_                =    "B18101C_006", # AIAN 18 to 64 with a disability
+                aian_dis_65p_                  =    "B18101C_009", # AIAN 65 plus with a disability
+
+# Native Hawaiian Pacific Islander (NHPI) disability
+
+                nhpi_dis_universe_             =    "B18101E_001", # Total NHPI civilian noninstitutionalized population
+                nhpi_dis_under18_              =    "B18101E_003", # NHPI under 18 with a disability
+                nhpi_dis_18_64_                =    "B18101E_006", # NHPI 18 to 64 with a disability
+                nhpi_dis_65p_                  =    "B18101E_009", # NHPI 65 plus with a disability
+
+# Other race disability
+
+                other_dis_universe_            =    "B18101F_001", # Total other civilian noninstitutionalized population
+                other_dis_under18_             =    "B18101F_003", # Other under 18 with a disability
+                other_dis_18_64_               =    "B18101F_006", # Other 18 to 64 with a disability
+                other_dis_65p_                 =    "B18101F_009", # Other 65 plus with a disability
+
+# Two or more race disability
+
+                twoplus_dis_universe_          =    "B18101G_001", # Total twoplus civilian noninstitutionalized population
+                twoplus_dis_under18_           =    "B18101G_003", # Twoplus under 18 with a disability
+                twoplus_dis_18_64_             =    "B18101G_006", # Twoplus 18 to 64 with a disability
+                twoplus_dis_65p_               =    "B18101G_009") # Twoplus 65 plus with a disability
+
+
+
+
+# Combine all variables into single vector
+
+
+total_acs_variables <- c(rent_burden,low_income_families,med_dis_earnings,disability)
+
+
+
+
+
+
+
+
+
+
+
+# Import data into single wide dataframe, remove margin of error (any variable with "_M" suffix)
+
+working_data <- get_acs(geography = "county",
+                        variables = total_acs_variables,
+                        state = statenumber,
+                        county = baycounties,
+                        year = acs_year,
+                        survey = acs_product,
+                        output = "wide") %>% 
+  select(-c(ends_with("_M"),GEOID)) 
+           
+
+
 
